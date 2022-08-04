f5b8366ec9c6a055fe32b646230d6d09a73d81f7
Debby <108422901+Debbbbbby@users.noreply.github.com>
2022-08-04 22:42:52 +0900

    220804 iOS 과외 과제 - AutoLayout으로 구현해보기
    220804 iOS 과외 과제 - AutoLayout으로 구현해보기
    구분을 위해  background 색만 칠해두었다

---
+++ KatalkProfileUI/KatalkProfileUI/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  KatalkProfileUI
+//
+//  Created by Debby on 2022/08/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
